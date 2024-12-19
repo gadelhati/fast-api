2254f5e2504cff28b5b6adffe8ca125f4d78e805
GadelhaTI <gadelha.ti@gmail.com>
2024-12-19 10:12:54 -0300

    test: add tests

---
+++ tests/testBook.py
@@ -0,0 +1,16 @@
+from fastapi.testbook import TestBook
+from app.main import app
+
+book = TestBook(app)
+
+def test_create_book():
+    _book = {"title": "testtitle", "description": "test description"}
+    response = book.post("/book", json=_book)
+    assert response.status_code == 201
+    assert response.json()["title"] == _book["title"]
+    assert response.json()["description"] == _book["description"]
+
+def test_get_book():
+    response = book.get("/book")
+    assert response.status_code == 200
+    assert len(response.json()) >= 1
