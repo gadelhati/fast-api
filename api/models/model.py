9b92768a66f3492ddd67235a49ddfa6151535807
GadelhaTI <gadelha.ti@gmail.com>
2024-12-19 18:38:41 -0300

    fix: change database mapping

--- api/models/model.py
+++ api/models/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, Table
 from sqlalchemy.orm import relationship
 from api.database import Base
 from datetime import datetime
@@ -10,6 +10,13 @@ class Book(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(50), nullable=False)
     description = Column(String(255), nullable=True)
+
+user_role = Table(
+    "user_role",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("role_id", Integer, ForeignKey("role.id"), primary_key=True)
+)
 
 class Role(Base):
     __tablename__ = "role"
@@ -30,4 +37,4 @@ class User(Base):
     secret = Column(String(150))
     start_datetime = Column(DateTime, default=datetime.utcnow)
 
-    roles = relationship("Role", cascade="all, delete-orphan")
+    roles = relationship("Role", secondary=user_role, back_populates="users")
