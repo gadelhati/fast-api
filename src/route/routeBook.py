1d29f5b7b30ab2cee63096923676ead33846bfb9
GadelhaTI <gadelha.ti@gmail.com>
2024-12-23 00:12:20 -0300

    refactor: change attribute name

--- src/route/routeBook.py
+++ src/route/routeBook.py
@@ -17,53 +17,53 @@ def get_db():
 @router.post("/")
 async def create(request: SchemaBook, db: Session=Depends(get_db)):
     try:
-        _book = ServiceBook.create(db, created=request)
-        return Response(code=201, status="Created", message="Created", result=_book).dict(exclude_none=True)
+        _result = ServiceBook.create(db, created=request)
+        return Response(code=201, status="Created", message="Created", result=_result).dict(exclude_none=True)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
 @router.patch("/")
 async def cancel(request: SchemaBook, db: Session=Depends(get_db)):
     try:
-        _book = ServiceBook.cancel(db, cancelled=request)
-        return Response(code=202, status="Accepted", message="Cancelled", result=_book).dict(exclude_none=True)
+        _result = ServiceBook.cancel(db, cancelled=request)
+        return Response(code=202, status="Accepted", message="Cancelled", result=_result).dict(exclude_none=True)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/")
 async def get(db: Session=Depends(get_db)):
     try:
-        _book = ServiceBook.get(db, 0, 100)
-        return Response(code=200, status="Ok", message="Finded all", result=_book).dict(exclude_none=True)
+        _result = ServiceBook.get(db, 0, 100)
+        return Response(code=200, status="Ok", message="Finded all", result=_result).dict(exclude_none=True)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/{id}")
 async def get_by_id(id: UUID, db: Session=Depends(get_db)):
     try:
-        _book = ServiceBook.get_by_id(db, id)
-        if not _book:
+        _result = ServiceBook.get_by_id(db, id)
+        if not _result:
             raise HTTPException(status_code=404, detail=f"Book {id} not found")
-        return Response(code=200, status="Ok", message="Finded one", result=_book).dict(exclude_none=True)
+        return Response(code=200, status="Ok", message="Finded one", result=_result).dict(exclude_none=True)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.put("/")
 async def update(request: SchemaBook, db: Session=Depends(get_db)):
     try:
-        _book = ServiceBook.update(db, updated=request)
-        if not _book:
+        _result = ServiceBook.update(db, updated=request)
+        if not _result:
             raise HTTPException(status_code=404, detail=f"Book {id} not found")
-        return Response(code=202, status="Accepted", message="Updated", result=_book).dict(exclude_none=True)
+        return Response(code=202, status="Accepted", message="Updated", result=_result).dict(exclude_none=True)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 @router.delete("/{id}")
 async def delete(id: UUID, db: Session=Depends(get_db)):
     try:
-        _book = ServiceBook.remove(db, id=id)
-        if not _book:
+        _result = ServiceBook.remove(db, id=id)
+        if not _result:
             raise HTTPException(status_code=404, detail=f"Book {id} not found")
-        return Response(code=204, status="No Content", message="Delete one", result=_book).dict(exclude_none=True)
+        return Response(code=204, status="No Content", message="Delete one", result=_result).dict(exclude_none=True)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
