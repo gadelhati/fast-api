fcc5e71db83a188d24a48f4425e70790835997fd
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2025-01-06 17:39:27 -0300

    fix: permit all

--- src/main.py
+++ src/main.py
@@ -23,7 +23,8 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["*"],
