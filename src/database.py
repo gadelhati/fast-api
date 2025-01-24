2ae1e0e5280614715ef5eb9a6b7978d4c72d11a1
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2025-01-24 09:36:49 -0300

    chore: change database url

--- src/database.py
+++ src/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgresql_e3u5_user:sGC82cam8LyB08Sj3x3SLIRgKq2bpiPx@dpg-cu3c4mhopnds73855c4g-a/postgresql_e3u5"
+DATABASE_URL = "postgresql://postgresql_e3u5_user:sGC82cam8LyB08Sj3x3SLIRgKq2bpiPx@dpg-cu3c4mhopnds73855c4g-a.oregon-postgres.render.com/postgresql_e3u5"
+# DATABASE_URL = "postgresql://postgresql_e3u5_user:sGC82cam8LyB08Sj3x3SLIRgKq2bpiPx@dpg-cu3c4mhopnds73855c4g-a/postgresql_e3u5"
 # DATABASE_URL = jdbc:postgresql://postgresql:efjbe46Ni4a6WC8i5h2Vn3FOtGwZQVTm@dpg-ctcd7etds78s739cdma0-a:5432/mfa_p68q
 # DATABASE_URL = "postgresql://postgres:G%40delha@localhost:5432/fastAPI"
 
