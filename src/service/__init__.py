b0b4cba4bc9a6d9a9bfffcb461bdb396a5b5df9e
GadelhaTI <gadelha.ti@gmail.com>
2025-07-21 14:59:12 -0300

    chore: cleaning up

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,16 +1,4 @@
-# services/__init__.py
 from .base import BaseService, ServiceException, NotFoundError, IntegrityError, ValidationError
 from .user import UserService
 from .role import RoleService
 from .permission import PermissionService
-
-__all__ = [
-    'BaseService',
-    'ServiceException',
-    'NotFoundError',
-    'IntegrityError',
-    'ValidationError',
-    'UserService',
-    'RoleService',
-    'PermissionService'
-]
