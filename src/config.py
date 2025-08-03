d8b228cca34ed9569e88264057d97d5c770d323b
GadelhaTI <gadelha.ti@gmail.com>
2025-08-03 00:15:10 -0300

    chore: add schema

--- src/config.py
+++ src/config.py
@@ -3,4 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DATABASE_SCHEMA = os.getenv("DATABASE_SCHEMA")
 DATABASE_URL = os.getenv("DATABASE_URL")
--- src/database.py
+++ src/database.py
@@ -1,13 +1,15 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncAttrs
-
-from src.config import DATABASE_URL
+from src.config import DATABASE_SCHEMA, DATABASE_URL
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=False)
+with engine.connect() as conn:
+    conn.execute(text(f"CREATE SCHEMA IF NOT EXISTS {DATABASE_SCHEMA}"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base(cls=AsyncAttrs)
+Base.metadata.schema = DATABASE_SCHEMA
 
 def get_db():
     db = SessionLocal()
