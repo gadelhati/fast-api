dccd4db959d855f2a7dd484e832ec5e00ca2ef50
GadelhaTI <gadelha.ti@gmail.com>
2025-08-06 00:07:27 -0300

    feature: add services

--- src/route/auth.py
+++ src/route/auth.py
@@ -44,3 +44,21 @@ async def authenticate_user(
         # "access_token": generate_jwt_token(user),
         # "token_type": "bearer"
     }
+
+
+@auth.post('/signup', summary="Create new user", response_model=UserOut)
+async def create_user(data: UserAuth):
+    # querying database to check if user already exist
+    user = db.get(data.email, None)
+    if user is not None:
+            raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this email already exist"
+        )
+    user = {
+        'email': data.email,
+        'password': get_hashed_password(data.password),
+        'id': str(uuid4())
+    }
+    db[data.email] = user    # saving user to database
+    return user
